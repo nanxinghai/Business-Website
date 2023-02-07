1ac107095afd8a7401d60c404a1bef301235a9b3
simon <2045698851@qq.com>
2023-02-07 17:57:55 +0800

    初始化登陆

--- 项目源码/front/zhiyun_sys_front/src/main.js
+++ 项目源码/front/zhiyun_sys_front/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// 引入初始化normalize.css
+import 'normalize.css/normalize.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ 项目源码/front/zhiyun_sys_front/src/router/index.js
@@ -0,0 +1,41 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+// 路由规则
+const route = [
+    {
+        name: 'Login',
+        path: '/login',
+        component: () => import('@/views/login/index.vue')
+    }   
+]
+
+const router = new VueRouter({
+    routes:route
+})
+
+/**
+ * 判断当前路由是否存在
+ * @param to
+ * @returns {boolean}
+ */
+ function hasRoute(to) {
+    let find = router.getRoutes().find(item => item.name === to.name);
+    return !!find;
+
+}
+
+// 全局前置路由
+router.beforeEach((to, from, next) => {
+    next()
+})
+
+// 全局后置路由
+router.afterEach((to,from) => {
+    // console.log("to",to)
+    // console.log("from",from)
+})
+
+export default router
