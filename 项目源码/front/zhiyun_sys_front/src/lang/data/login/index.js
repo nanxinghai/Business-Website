e8fe230627102eb4cb108066e3996c98e905a598
simon <2045698851@qq.com>
2023-02-14 18:02:45 +0800

    登录页面制作中

--- 项目源码/front/zhiyun_sys_front/src/lang/data/login/index.js
+++ 项目源码/front/zhiyun_sys_front/src/lang/data/login/index.js
@@ -1,19 +1,41 @@
 const info = {
   ZH: {
     title: '登录',
-    subTitle1: '密码登陆',
-    subTitle2: '验证码登陆',
+    subTitle1: '密码登录',
+    subTitle2: '验证码登录',
+    // 密码登录
     passPlaceHolder1: '邮箱/手机号',
     passPlaceHolder2: '密码',
+    forgetPass: '忘记密码',
     passTips: '请输入格式正确的邮箱或手机号',
+    // 验证码登录
+    codePlaceHolder1: '手机号',
+    codePlaceHolder2: '验证码',
+    getCode: '立即发送',
+    codeTips: '请输入手机号验证码',
+    // 微信登录
+    wxTips: '请使用微信扫码登录',
+    otherLoginMethod: '第三方登录'
   },
   EN: {
     title: 'Login',
     subTitle1: 'Password login',
     subTitle2: 'Verification code login',
+    // 密码登录
     passPlaceHolder1: 'Email/Phone number',
     passPlaceHolder2: 'Password',
+    forgetPass: 'Forgot password',
     passTips: 'Please enter the correct email or mobile number',
+    // 验证码登录
+    codePlaceHolder1: 'Phone number',
+    codePlaceHolder2: 'Verification Code',
+    getCode: 'Send Now',
+    codeTips: 'Please enter the mobile phone number verification code',
+    // 微信登录
+    wxTips: 'Please log in using WeChat scanning code',
+
+
+    otherLoginMethod: 'Third-party login'
   }
 }
 export default info
--- 项目源码/front/zhiyun_sys_front/src/views/login/index.vue
+++ 项目源码/front/zhiyun_sys_front/src/views/login/index.vue
@@ -11,25 +11,64 @@
       </div>
     </div>
     <div class="body">
+      <!-- 左图 -->
       <div class="left_img">
         <img :src="require('@/assets/img/login_bg.png')" alt="">
       </div>
+      <!-- 右盒子 -->
       <div class="right_box">
-        <div class="login_box">
+        <div class="login_box" v-if="loginMethod === 'phoneLogin'">
           <h1>{{$t('login.title')}}</h1>
+          <!-- 副标题 -->
           <div class="sub_title">
-            <span class="active">{{$t('login.subTitle1')}}</span>
-            <span class="two">{{$t('login.subTitle2')}}</span>
+            <span :class="{'active':subLogin}" @click="switchSubLogin(true)">{{$t('login.subTitle1')}}</span>
+            <span class="two" :class="{'active':!subLogin}" @click="switchSubLogin(false)">{{$t('login.subTitle2')}}</span>
           </div>
-          <div class="login_form">
+          <!-- 密码登录表单 -->
+          <div class="login_form" v-if="subLogin">
             <form style="height:100%;width:100%;">
-              <input type="text" :placeholder="$t('login.passPlaceHolder1')" style="margin-top: 13%;"/>
-              <input type="text" :placeholder="$t('login.passPlaceHolder2')"/>
+              <input type="text" :placeholder="$t('login.passPlaceHolder1')" style="margin-top: 14%;"/>
+              <div class="pass">
+                <input type="password" :placeholder="$t('login.passPlaceHolder2')"/>
+                <div>{{$t('login.forgetPass')}}</div>
+              </div>
               <div class="tips_box">{{$t('login.passTips')}}</div>
               <input  type="button" :value="$t('login.title')"/>
             </form>
           </div>
-          <div class="login_method"></div>
+          <!-- 验证码登录表单 -->
+          <div class="login_form" v-if="!subLogin">
+            <form style="height:100%;width:100%;">
+              <input type="text" :placeholder="$t('login.codePlaceHolder1')" style="margin-top: 14%;"/>
+              <div class="pass">
+                <input type="password" :placeholder="$t('login.codePlaceHolder2')"/>
+                <div>{{$t('login.getCode')}}</div>
+              </div>
+              <div class="tips_box">{{$t('login.codeTips')}}</div>
+              <input  type="button" :value="$t('login.title')"/>
+            </form>
+          </div>
+          <!-- 第三方登录 -->
+          <div class="login_method">
+            <div class="other_method">{{$t('login.otherLoginMethod')}}</div>
+            <div class="method_icon">
+              <img :src="require('@/assets/img/sign-weixin.png')" alt="" width="100%">
+            </div>
+          </div>
+        </div>
+        <!-- 微信扫描 -->
+        <div class="login_box" v-if="loginMethod === 'wxchatLogin'">
+          <div class="tips">{{$t('login.wxTips')}}</div>
+          <!-- 二维码 -->
+          <div class="login_form">
+
+          </div>
+          <div class="login_method">
+            <div class="other_method">{{$t('login.otherLoginMethod')}}</div>
+            <div class="method_icon">
+              <img :src="require('@/assets/img/sign-phone.png')" alt="" width="100%">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -40,7 +79,19 @@
 import ChangeLang from '@/components/common/changeLang.vue'
 export default {
     name:'Login',
-    components:{ChangeLang}
+    components:{ChangeLang},
+    data(){
+      return {
+        subLogin: true,
+        loginMethod: 'wxchatLogin'
+      }
+    },
+    methods:{
+      // 切换
+      switchSubLogin(value){
+        this.subLogin = value
+      }
+    },
 }
 </script>
 
@@ -83,7 +134,7 @@ export default {
     justify-content: space-evenly;
     &>div {
       width: 30%;
-      height: 60%;
+      height: 55%;
     }
     .left_img {
       img {
@@ -100,7 +151,7 @@ export default {
         margin: 0 auto;
         width: 70%;
         height: 100%;
-        background-color: white;
+        background-color: #FFFFFF;
         border-radius: 5%;
         box-shadow: 4px 4px 10px 4px rgb(0 0 0 / 20%);
         .sub_title {
@@ -110,7 +161,7 @@ export default {
           overflow: hidden;
           white-space: nowrap;
           &>span {
-            // float: left;
+            cursor: pointer;
             display: inline-block;
             height: 30px;
             font-size: @font_size_large;
@@ -128,22 +179,77 @@ export default {
         .login_form {
           width: 100%;
           height: 60%;
-          input[type="text"] {
+          border-bottom: 1px #F0F0F0 solid;
+          input[type="text"],input[type="password"] {
             width: 100%;
-            height: 13%;
+            height: 16%;
             border-radius: 6px;
             border: 2px #096DD9 solid;
-            margin-top: 7%;
+            margin-top: 8%;
             padding-left: 3%;
             box-sizing: border-box;
           }
-          input[type="text"]:focus {
+          input[type="text"]:focus,input[type="password"]:focus {
             outline: 0;
             border: 2px #096DD9 solid;
           }
+          .pass {
+            width: 100%;
+            height: 16%;
+            overflow: hidden;
+            margin-top: 8%;
+            position: relative;
+            &>input[type="password"] {
+              margin-top:0;
+              height: 100%;
+            }
+            &>div {
+              cursor: pointer;
+              color: black;
+              font-size: @font_size_small;
+              position: absolute;
+              right: 5%;
+              top:50%;
+              transform: translateY(-50%);
+            }
+          }
+          .tips_box {
+            color: @font_color_red;
+            font-size: @font_size_ssmall;
+            margin-top: 8%;
+          }
+          input[type="button"] {
+            cursor: pointer;
+            width: 100%;
+            height: 16%;
+            background-color: #096DD9;
+            box-sizing: border-box;
+            border-radius: 6px;
+            border: 1px #096DD9 solid;
+            color: #FFFFFF;
+            margin-top: 8%;
+          }
         }
         .login_method {
-
+          height: 8%;
+          overflow: hidden;
+          margin-top: 7%;
+          .other_method {
+            float: left;
+            font-size: @font_size_small;
+            color: @font_color_grey;
+            height: 100%;
+            line-height: 40px;
+          }
+          .method_icon {
+            cursor: pointer;
+            box-sizing: border-box;
+            float: right;
+            height: 40px;
+            width: 40px;
+            border: 1px #F0F0F0 solid;
+            border-radius: 50%;
+          }
         }
       }
     }
